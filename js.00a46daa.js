cee51bbfa2823aae8d3eed8bbe3c37bd21ec2c73
grits-tatiana <gritsenko.ty@yandex.ru>
2019-12-05 15:35:54 +0300

    Updates

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -158,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63125" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63418" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
